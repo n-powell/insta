96c04c11cbdaefd5df8c65c55e8b97c44bee2546
Aubrey Masten and Nick Powell <aubrey+nick@>
2017-07-06 10:59:17 -0700

    add sign out link

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -17,6 +17,9 @@ class ImagesController < ApplicationController
   end
 
   def index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
     @images = Image.all
     @user = current_user
   end
